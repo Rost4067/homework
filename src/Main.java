5d4dced264d961e8355da2416b6aca0625984f2f
Professional <morewrost@gmail.com>
2022-10-20 18:31:15 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        int age = 18;
+        if (age < 0) {
+            System.out.println("become an adult");
+
+        } else if (age > 18) {
+            System.out.println("become an adult");
+        }
+        else {
+            System.out.println("underage");
+        }
     }
 }
